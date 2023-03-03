94de90af3cdce5afdeda5318db2aa4b13b6c4e41
seock1000 <tjrcjs0705@gmail.com>
2023-03-03 19:51:47 +0900

    초기 잘못된 코드

---
+++ src/user/dao/UserDao.java
@@ -0,0 +1,44 @@
+package user.dao;
+
+import user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/tobi_spring", "root", "0512");
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into user(id, name, password) values(?,?,?)"
+        );
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/tobi_spring", "root", "0512");
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from user where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        return user;
+    }
+}
