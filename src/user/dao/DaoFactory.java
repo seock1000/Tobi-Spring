867ae6b93b269631e6ed10317274f0ae4a2cc871
seock1000 <tjrcjs0705@gmail.com>
2023-03-06 11:38:26 +0900

    오브젝트 팩토리 중복 코드 생생 및 분리

--- src/user/dao/DaoFactory.java
+++ src/user/dao/DaoFactory.java
@@ -5,8 +5,25 @@ package user.dao;
  */
 public class DaoFactory {
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
+        ConnectionMaker connectionMaker = connectionMaker();
         UserDao userDao = new UserDao(connectionMaker);
         return userDao;
+    }
+
+    public AccountDao accountDao(){
+        ConnectionMaker connectionMaker = connectionMaker();
+        AccountDao accountDao = new AccountDao(connectionMaker);
+        return accountDao;
+    }
+
+    public MessageDao messageDao() {
+        ConnectionMaker connectionMaker = connectionMaker();
+        MessageDao messageDao = new MessageDao(connectionMaker);
+        return messageDao;
+    }
+
+    // ConnectionMaker 타입 생성코드 중복제거
+    private ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
---
+++ src/user/dao/MessageDao.java
@@ -0,0 +1,9 @@
+package user.dao;
+
+public class MessageDao {
+    private ConnectionMaker connectionMaker;
+
+    public MessageDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+}
