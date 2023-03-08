28e5aa749a90fd413f47e3dd806a4d6f0006dde9
seock1000 <tjrcjs0705@gmail.com>
2023-03-08 18:15:20 +0900

    count 테스트 메서드 추가

--- src/test/user/UserDaoTest.java
+++ src/test/user/UserDaoTest.java
@@ -2,10 +2,8 @@ package test.user;
 
 import org.junit.After;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.transaction.annotation.Transactional;
 import user.dao.DaoFactory;
 import user.dao.UserDao;
 import user.domain.User;
@@ -26,6 +24,9 @@ public class UserDaoTest {
         dao.deleteAll();
     }
 
+    /**
+     * 테스트 동일 결과 보장
+     */
     @Test
     public void addAndGet() throws ClassNotFoundException, SQLException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
@@ -47,5 +48,30 @@ public class UserDaoTest {
 
         assertThat(getUser.getName(), is(user.getName()));
         assertThat(getUser.getPassword(), is(user.getPassword()));
+    }
+
+    /**
+     * getCount() 메서드 테스트
+     */
+    @Test
+    public void count() throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("ronnie", "염석천", "springno1");
+        User user2 = new User("wonu", "김원우", "springno2");
+        User user3 = new User("gunuk", "함건욱", "springno3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 }
